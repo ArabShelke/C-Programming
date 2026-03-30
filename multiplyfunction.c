60073ff0ce12b542e83f45353da1adae1253805b
ArabShelke <arabshelke7499@gmail.com>
2026-03-31 01:31:02 +0530

    mutiplyfun.c

---
+++ multiplyfunction.c
@@ -0,0 +1,33 @@
+// //WITHOUT USER INPUT
+// #include<stdio.h>
+// int multiply(int a, int b)
+// {
+//     return a*b;
+// }
+// int main(int argc, char const *argv[])
+// {
+//     int result = multiply(3,4);
+//     printf("%d\n",result);
+//     return 0;
+
+//  }
+
+
+// //WITH USER INPUT
+// #include<stdio.h>
+// int multiply(int a, int b)
+// {
+//     return a*b;
+// }
+// int main(int argc, char const *argv[])
+// {
+//     int x, y, result;
+//     printf("enter 1st number:");
+//     scanf("%d",&x);
+//     printf("enter 2nd number:");
+//     scanf("%d",&y);
+//     result = multiply(x, y);
+//     printf("so multiplication is %d\n", result);
+//     return 0;
+
+// }
