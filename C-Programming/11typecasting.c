b0eeed446712a9ae80cebf2771609e1badf4b501
ArabShelke <arabshelke7499@gmail.com>
2026-03-31 22:57:22 +0530

    typecasting

---
+++ C-Programming/11typecasting.c
@@ -0,0 +1,91 @@
+//TYPECASTING\\
+
+/*#include<stdio.h>
+
+int main()
+{
+    int a = 5,b = 2;
+    float result;
+
+    result = (float)a / b;
+
+    printf("Result = %f", result);
+
+    return 0;
+}*/
+
+
+//|| IMPLICIT||\\
+
+/*#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a = 10;
+    float b;
+    b = a;
+    printf("%f", b);
+    return 0;
+}
+*/
+
+
+//|| EXPLICIT||\\
+
+/*#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    float x = 9.8;
+    int y;
+    y = (int)x;
+    
+    printf("%d", y);
+    return 0;
+}*/
+
+
+
+/*Write a C program to divide two integers 
+and display the result in floating-point 
+using typecasting.
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a = 10, b = 3;
+   float result;
+
+    result = (float)a/b;
+    printf("result = %f", result);
+
+
+    return 0;
+}*/
+
+
+/*Write a C program to convert a float value
+into an integer using typecasting
+#include<stdio.h>
+int main()
+{
+    float num = 12.35;
+    int x;
+
+    x = (int)num;
+    printf("enter integer value of x = %d", x);
+    return 0;
+}*/
+
+
+
+/*write a C program to find the average of
+two numbers using typecasting
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=10, b=3;
+    float avg;
+    avg = (float)(a+b)/2;
+    printf("average = %f", avg);
+
+    
+    return 0;
+}*/
