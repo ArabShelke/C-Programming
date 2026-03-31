cb0b4c7aae2302cbc6c64b8c0c060b41699ce827
ArabShelke <arabshelke7499@gmail.com>
2026-04-01 03:20:56 +0530

    function-prototype

---
+++ 13functionprototype.c
@@ -0,0 +1,70 @@
+// Add two numbers using function prototype
+
+// #include<stdio.h>
+// int sum(int,int);
+// int main(int argc, char const *argv[])
+// {
+//     int a, b, result;
+//     printf("Enter two numbers: ");
+//     scanf("%d %d", &a, &b);
+//     result = sum(a, b);
+//     printf("Sum = %d\n", result);
+//     return 0;
+// }
+// int sum(int x, int y)
+// {
+//     return x + y;
+// }
+
+
+
+
+// Even or Odd using function prototype
+
+
+// #include<stdio.h>
+// int check(int);
+// int main(int argc, char const *argv[])
+// {
+//     int num;
+//     printf("enter a number:");
+//     scanf("%d", &num);
+//     check(num);
+//     return 0;
+// }
+
+// int check(int n)
+// {
+//     if (n%2 == 0)
+//     {
+//         printf("even\n");
+//     }
+//     else
+//     {
+//         printf("odd\n");
+//     }
+    
+// }
+
+
+
+// Find square using function prototype
+
+
+// #include<stdio.h>
+// int square(int);
+
+// int main(int argc, char const *argv[])
+// {
+//     int num, result;
+//     printf("enter number you want square of ");
+//     scanf("%d", &num);
+//     result = square(num);
+//     printf("Square = %d\n", result);
+//     return 0;
+// }
+
+// int square(int n)
+// {
+//     return n * n;
+// }
