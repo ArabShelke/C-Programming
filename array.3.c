bfd9aa10558b9851f86bbceceba7be7eb31ef29c
ArabShelke <arabshelke7499@gmail.com>
2026-04-08 23:54:18 +0530

    more codes on array 3

---
+++ array.3.c
@@ -0,0 +1,49 @@
+// Count Frequency of an Element
+/*#include <stdio.h>
+
+int main() {
+    int arr[5] = {2, 3, 2, 5, 2};
+    int key = 2, count = 0;
+
+    for(int i = 0; i < 5; i++) {
+        if(arr[i] == key) {
+            count++;
+        }
+    }
+
+    printf("Frequency = %d", count);
+
+    return 0;
+} */
+
+//Print Elements in Reverse Order
+/*#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    for(int i = 4; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}*/
+
+
+//Sum of Even Numbers in Array
+/*#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int sum = 0;
+
+    for(int i = 0; i < 5; i++) {
+        if(arr[i] % 2 == 0) {
+            sum += arr[i];
+        }
+    }
+
+    printf("Sum of even numbers = %d", sum);
+
+    return 0;
+}*/
