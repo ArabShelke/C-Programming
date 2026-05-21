c5020fdc13e8eb0c450c12039128c52a31a5dd1d
ArabShelke <arabshelke7499@gmail.com>
2026-05-21 21:39:18 +0530

    ass1.py

---
+++ ass.1.py
@@ -0,0 +1,23 @@
+# Basic Calculator Program
+
+# Taking input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Arithmetic Operations
+add = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+modulus = num1 % num2
+power = num1 ** num2
+
+# Displaying Results
+print("\n----- Results -----")
+
+print("Addition =", add)
+print("Subtraction =", subtract)
+print("Multiplication =", multiply)
+print("Division =", divide)
+print("Modulus =", modulus)
+print("Power =", power)
