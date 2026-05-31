1e5238ecbc4e385447b675de192a6a2146b92fc1
ArabShelke <arabshelke7499@gmail.com>
2026-05-31 12:16:14 +0530

    revision.1

---
+++ Revision/Revision.1.c
@@ -0,0 +1,109 @@
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int a, b;
+//     printf("enter number a ");
+//     scanf("%d", &a);
+//     printf("enter number b ");
+//     scanf("%d", &b);
+//     printf("the sum is %d\n" , a + b); 
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     printf("%lu", sizeof(int)); 
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int a, b;
+//     char op;
+    
+//     printf("enter first number : ");
+//     scanf("%d", &a);
+
+//     printf("enter opertor(+, -, *, /): ");
+//     scanf(" %c", &op);
+    
+//     printf("enter second number : ");
+//     scanf("%d", &b);
+
+//     if(op == '+')
+//     {
+//         printf("answer = %d", a + b);
+//     }
+//     else if(op == '-')
+//     {
+//         printf("answer = %d", a - b);
+//     }
+//     else if(op == '*')
+//     {
+//         printf("answer = %d", a * b);
+//     }
+//     else if(op == '/')
+//     {
+//         if (b != 0)
+        
+//             printf("answer = %d", a / b);
+//             else
+            
+//                 printf(" %d cannot divide by zero", a);
+            
+        
+        
+//     }
+//     else
+//     {
+//         printf("invalid operator");
+//     }
+    
+//     return 0;
+// }
+
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a, b;
+//     char op;
+
+//     printf("Enter first number: ");
+//     scanf("%d", &a);
+
+//     printf("Enter operator (+, -, *, /): ");
+//     scanf(" %c", &op);
+
+//     printf("Enter second number: ");
+//     scanf("%d", &b);
+
+//     if(op == '+')
+//     {
+//         printf("Answer = %d", a + b);
+//     }
+//     else if(op == '-')
+//     {
+//         printf("Answer = %d", a - b);
+//     }
+//     else if(op == '*')
+//     {
+//         printf("Answer = %d", a * b);
+//     }
+//     else if(op == '/')
+//     {
+//         printf("Answer = %d", a / b);
+//     }
+//     else
+//     {
+//         printf("Invalid Operator");
+//     }
+
+//     return 0;
+// }
