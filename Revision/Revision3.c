bfe4025117be0530bd9663702cc4aa0c42194765
ArabShelke <arabshelke7499@gmail.com>
2026-06-01 04:48:51 +0530

    Create Revision3.c

---
+++ Revision/Revision3.c
@@ -0,0 +1,51 @@
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int age;
+//     printf("enter your age : ");
+//     scanf("%d", &age);
+//     printf("you have entered %d as your age\n", age);
+//     if (age>=18)
+//     {
+//         printf("you can vote ");
+//     }
+//     else{
+//         printf("you can not ");
+//     }
+    
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int maths, science;
+//     printf("enter your marks in maths : ");
+//     scanf("%d", &maths);
+//     printf("enter your marks in science : ");
+//     scanf("%d", &science);
+//     if (maths>=33 && science>=33)
+//     {
+//         printf("your gift is 45 cause you passed both");
+//     }
+//     else if (maths>=33)
+//     {
+//          printf("your gift is 15 cause you passed maths.");
+
+//     }
+//     else if (science>=33)
+//     {
+//          printf("your gift is 15 cause you passed science");
+//     }
+//     else{
+//          printf("no gift for you.");
+//     }
+    
+    
+    
+
+//     return 0;
+// }
+
+
