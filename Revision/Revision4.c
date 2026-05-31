403b7869161f1a3caa549e9cd24ea94751bda047
ArabShelke <arabshelke7499@gmail.com>
2026-06-01 04:48:57 +0530

    Create Revision4.c

---
+++ Revision/Revision4.c
@@ -0,0 +1,22 @@
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int age;
+//     printf("enter your age : ");
+//     scanf("%d", &age);
+//     switch (age)
+//     {
+//     case 3:
+//         printf("the age 3");
+//         break;
+//     case 13:
+//         printf("the age is 13");
+//         break;
+
+//     default:
+//     // printf("age is not 3 and 13 ")
+//         // break;
+//     }
+//     return 0;
+// }
+
