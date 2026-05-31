1bea6abcd7c63a56f46f034592718088475e33e5
ArabShelke <arabshelke7499@gmail.com>
2026-06-01 04:48:47 +0530

    Create Revision2.c

---
+++ Revision/Revision2.c
@@ -0,0 +1,39 @@
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int num;
+//     printf("Enter the the number you want multiplication of :");
+//     scanf("%d", &num);
+//     printf("The multiplication table of %d\n", num);
+//     printf("%d * 1 = %d\n", num, num * 1 );
+//     printf("%d * 2 = %d\n", num, num * 2 );
+//     printf("%d * 3 = %d\n", num, num * 3 );
+//     printf("%d * 4 = %d\n", num, num * 4 );
+//     printf("%d * 5 = %d\n", num, num * 5 );
+//     printf("%d * 6 = %d\n", num, num * 6 );
+//     printf("%d * 7 = %d\n", num, num * 7 );
+//     printf("%d * 8 = %d\n", num, num * 8 );
+//     printf("%d * 9 = %d\n", num, num * 9 );
+//     printf("%d * 10 = %d\n",num, num * 10);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// #define PI 3.14
+// int main(int argc, char const *argv[])
+// {
+//     int a = 8;
+//     const float b = 7.333;
+//     printf("tab character \t\t my hero \a %f", PI);
+//     return 0;
+// }/
+
+
+#include <windows.h>
+
+int main()
+{
+    Beep(2000, 4000);
+    return 0;
+}
