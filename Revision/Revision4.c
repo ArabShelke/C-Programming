f61259b1f2ff78280968ccc506199f9afe158d9b
ArabShelke <arabshelke7499@gmail.com>
2026-06-02 03:27:45 +0530

    Update Revision4.c

--- Revision/Revision4.c
+++ Revision/Revision4.c
@@ -20,3 +20,39 @@
 //     return 0;
 // }
 
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int i, age;
+//     for ( i = 0; i < 10; i++)
+//     {
+//         printf("enter your age %d\n", i);
+//         scanf("%d", &age);
+//         if (age > 10)
+//         {
+//             printf("entered age is above 10 ");
+//             break;
+//         }
+        
+//     }
+    
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     label:
+//     printf("we are inside label\n");
+//     goto end;
+//     printf("hello waorld\n");
+//     goto label;
+//     end:
+//     printf("we are at end ");
+
+//     return 0;
+// }   
+
+ 
