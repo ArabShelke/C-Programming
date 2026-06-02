587933a895f495c7260ce32e6746aa266e07a2b2
ArabShelke <arabshelke7499@gmail.com>
2026-06-02 05:41:15 +0530

    Update Revision6.c

--- Revision/Revision6.c
+++ Revision/Revision6.c
@@ -1,8 +1,126 @@
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int a = 3;
+//     float b = (float )54/5;
+//     printf("the value of a is %f\n", b);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int sum(int a, int b)
+// {
+//     return a + b;
+// }
+// int main(int argc, char const *argv[])
+
+// {
+//     int a, b, c;
+    
+//     printf(" enter no. a : ");
+//     scanf("%d", &a);
+    
+//     printf(" enter no. b : ");
+//     scanf("%d", &b);
+//     c = sum(a, b);
+//     printf("the sum is %d\n", c);
+
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// void name()
+//     {
+//         printf("my name is aayu ");
+//     }
+    
+// int main(int argc, char const *argv[])
+//     {
+//         name();
+//         return 0;
+//     }
+    
+
+
+// #include<stdio.h>
+// void sum (int a, int b)
+// {
+//     int c = a+b;
+//     printf(" the sum is %d\n", c);
+// }
+// int main(int argc, char const *argv[])
+// {
+//     sum(4, 5);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int getvalue()
+// {
+//     int x = 10;
+//     return x;
+// }
+// int main(int argc, char const *argv[])
+// {
+//     int num;
+//     num = getvalue();
+//     printf("num = %d", num);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// void message()
+// {
+//     printf("hello duniya");
+// }
+// int main(int argc, char const *argv[])
+// {
+//     message();
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int multiply(int a, int b)
+// {
+//     return a*b;
+// }
+// int main(int argc, char const *argv[])
+// {
+//     int a, b, c;
+//     printf("enter 1st no : ");
+//     scanf("%d", &a);
+   
+//     printf("enter 2nd no : ");
+//     scanf("%d", &b);
+//     c = multiply(a, b);
+//     printf("multiplication = %d", c);
+//     return 0;
+// }
+
+
 #include<stdio.h>
+int check(int);
 int main(int argc, char const *argv[])
 {
-    int a = 3;
-    float b = (float )54/5;
-    printf("the value of a is %f\n", b);
+    int num;
+    printf("enter a number : ");
+    scanf("%d", &num);
+    check(num);
     return 0;
+}
+int check(int n)
+{
+    if (n/2 == 0)
+    {
+        printf("even number");
+    }
+    else{
+        printf("odd number");
+    }
+    
+
 }
