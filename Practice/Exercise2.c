7bd93bac1b9565e283fdf5a5155d4ac18f3724a3
ArabShelke <arabshelke7499@gmail.com>
2026-06-05 00:27:56 +0530

    Create Exercise2.c

---
+++ Practice/Exercise2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    float kmsToMiles = 0.621371;
+    float inchesToFoot = 0.0833333; 
+    float cmsToInches = 0.393701; 
+    float poundTokgs = 0.453592;
+    float inchesTometers = 0.0254;
+    
+    
+
+    return 0;
+}
