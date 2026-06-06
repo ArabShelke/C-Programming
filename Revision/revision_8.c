d28a8b1bb76acd8ee219325271a98dead3b6b7f2
ArabShelke <arabshelke7499@gmail.com>
2026-06-07 04:25:25 +0530

    Update revision_8.c

--- Revision/revision_8.c
+++ Revision/revision_8.c
@@ -9,22 +9,46 @@
 
 
 
-#include<stdio.h>
-int main(int argc, char const *argv[])
-{
-    int marks[4];
+//#include<stdio.h>
+//int main(int argc, char const *argv[])
+// {
+//     int marks[4];
 
-    for (int i = 0; i < 4; i++)
-    {
-        printf("enter the value of %d element of the array\n", i);
-        scanf("%d", &marks[i]);
-    }
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("enter the value of %d element of the array\n", i);
+//         scanf("%d", &marks[i]);
+//     }
 
-    for (int i = 0; i < 4; i++)
-    {
-        printf("the value of %d element of the array is %d\n", i, marks[i]);
-    }
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("the value of %d element of the array is %d\n", i, marks[i]);
+//     }
     
     
-    return 0;
-}
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int arr[10] = {1,2,3,45};
+//     printf("%d", arr[3]);
+
+
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int arr[] = {1,2,3,45,6,7};
+//     arr[2] = 456;
+//     printf("%d", arr[2]);
+//     return 0;
+// }
+
+
