05bc6c72cc3e62e1443b2aa5b3dd4805e6ffcaa3
ArabShelke <arabshelke7499@gmail.com>
2026-06-08 08:58:05 +0530

    Update arrays_3.c

--- Arrays/arrays_3.c
+++ Arrays/arrays_3.c
@@ -1,18 +1,15 @@
 //Linear Search
 /*#include <stdio.h>
-
 int main() {
     int arr[5] = {3, 7, 2, 9, 5};
     int key = 9;
     int found = 0;
-
     for(int i = 0; i < 5; i++) {
         if(arr[i] == key) {
             found = 1;
             break;
         }
     }
-
     if(found)
         printf("Element found");
     else
@@ -24,7 +21,6 @@ int main() {
 
 //Copy One Array to Another
 /*#include <stdio.h>
-
 int main() {
     int arr1[5] = {1, 2, 3, 4, 5};
     int arr2[5];
@@ -40,20 +36,17 @@ int main() {
     return 0;
 }*/
 
-//Find Smallest Element
-#include <stdio.h>
 
+//Find Smallest Element
+/*#include <stdio.h>
 int main() {
     int arr[5] = {12, 5, 8, 2, 10};
     int min = arr[0];
-
     for(int i = 1; i < 5; i++) {
         if(arr[i] < min) {
             min = arr[i];
         }
     }
-
     printf("Smallest = %d", min);
-
     return 0;
-}
+}*/
