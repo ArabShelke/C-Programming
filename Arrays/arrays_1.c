c1ae1c61901710353a66ca739fbd0eb98cfb34bc
ArabShelke <arabshelke7499@gmail.com>
2026-06-08 08:57:50 +0530

    Update arrays_1.c

--- Arrays/arrays_1.c
+++ Arrays/arrays_1.c
@@ -1,9 +1,7 @@
 // Accessing an array element
-
 // #include<stdio.h>
 // int main(int argc, char const *argv[])
-// {
-    
+// {    
 //     int arr[] = {1, 3, 6, 8};
 //     printf("%d", arr[3]);
 //     return 0;
@@ -14,8 +12,6 @@
 
 
 // Changing an array element
-
-
 // #include<stdio.h>
 // int main()
 // {int arr[] = {1, 3, 4, 5};
@@ -23,7 +19,6 @@
 // printf("%d", arr[2]);
 // return 0;
 // }
-
 
 
 
@@ -42,29 +37,24 @@
 //     for ( i = 0; i < 5; i++)
 //     {
 //         printf("%d ", arr[i]);
-//     }
-    
+//     }  
 //     return 0;
 // }
 
 
+
+
 //Write a C program to find sum of 5 numbers using array.
-
-#include<stdio.h>
-
-int main()
-{
-    int arr[5], i, sum = 0;
-
-    printf("Enter 5 numbers:\n");
-
-    for(i = 0; i < 5; i++)
-    {
-        scanf("%d", &arr[i]);
-        sum = sum + arr[i];
-    }
-
-    printf("Sum = %d", sum);
-
-    return 0;
-}
+// #include<stdio.h>
+// int main()
+// {
+//     int arr[5], i, sum = 0;
+//     printf("Enter 5 numbers:\n");
+//     for(i = 0; i < 5; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//         sum = sum + arr[i];
+//     }
+//     printf("Sum = %d", sum);
+//     return 0;
+// }
