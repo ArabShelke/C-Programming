5cb62460c0e5bf96f594cc86a9daec77df248c9b
ArabShelke <arabshelke7499@gmail.com>
2026-06-10 05:44:03 +0530

    Update revision_9.c

--- Revision/revision_9.c
+++ Revision/revision_9.c
@@ -59,19 +59,56 @@
 
 
 
-#include<stdio.h>
-int main(int argc, char const *argv[])
-{
-    char str1[50], str2[50];
-    int i = 0;
-    printf("enter string: \n");
-    fgets(str1, 50, stdin);
-    while (str1[i] != '\0')
-    {
-        str2[i] = str1[i];
-        i ++ ;
-    }
-    str2[i] = '\0';
-    printf("copied string = %s", str2);
-    return 0;
-}
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     char str1[50], str2[50];
+//     int i = 0;
+//     printf("enter string: \n");
+//     fgets(str1, 50, stdin);
+//     while (str1[i] != '\0')
+//     {
+//         str2[i] = str1[i];
+//         i ++ ;
+//     }
+//     str2[i] = '\0';
+//     printf("copied string = %s", str2);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     char str1[50], str2[50];
+//     int i = 0, flag = 0;
+
+//     printf("enter 1st string");
+//     fgets(str1, 50, stdin);
+   
+//     printf("enter 2nd string");
+//     fgets(str2, 50, stdin);
+    
+//     while (str1[i] != '\0' && str2[i] != '\0')
+//     {
+//         if (str1[i] != str2[i])
+//         {
+//             flag = 1;
+//             break;
+//         }
+//         i ++;
+//     }
+
+//         if (flag == 0)
+//         {
+//             printf("strings are equal");
+//         }
+//         else
+//         {
+//             printf("strigs are not equal");
+//         }
+//     return 0;
+// }
+
+
+
