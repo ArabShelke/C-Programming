114bf212dbbce5c7b05f423d45dadfaaf2d83609
ArabShelke <arabshelke7499@gmail.com>
2026-06-16 01:47:01 +0530

    Create Practice_6.c

---
+++ Practice/Practice_6.c
@@ -0,0 +1,29 @@
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int a =76;
+//     int *ptra = &a;
+//     printf("the value of a is %d", a);
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int a =76;
+//     int *ptra = &a;
+//     printf("the value of a is %d", *ptra);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int a = 76;
+//     int* ptra = &a;
+//     printf("the  address of a is %x", ptra);
+//     return 0;
+// }
