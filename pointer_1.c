3dd905cd63b90a6d6bfc2cfb9b8179017cd4edff
ArabShelke <arabshelke7499@gmail.com>
2026-06-16 01:46:53 +0530

    Create pointer_1.c

---
+++ pointer_1.c
@@ -0,0 +1,25 @@
+//print the value using '*' or a pointer
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int a = 76;
+//     int *ptra = &a;
+//     printf("the value of a is %d", *ptra);
+//     printf("the value of a is %d", a);
+//     return 0;
+// }
+
+
+
+//print the address of a
+// #include<stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int a = 87;
+//     int *ptra = &a;
+//     printf("the address of a is %x", ptra);
+//     printf("the address of a is %x", &a);
+//     printf("address of ptr %p", &ptra);
+
+//     return 0;
+// }
